4762cca59eba133ad033fff190af7a0cee49ec20
trojan-xs <49181542+trojan-xs@users.noreply.github.com>
2023-12-19 21:37:07 -0800

    Add files via upload

---
+++ 1update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Sys Update
+sudo printf "Root password:\n"
+
+printf "Updating and upgrading \n\n"
+sudo apt-get update
+sudo apt-get upgrade 
+
+printf "Creating directory to link to nginx volume\n\n"
+sudo mkdir /usr/share/nginx/html/
+wget -P /usr/share/nginx/html/ https://raw.githubusercontent.com/trojan-xs/static-hello/main/index.html
+sudo mkdir /var/lib/docker/volumes/ngx-proxy/letsencrypt
+sudo mkdir /var/lib/docker/volumes/ngx-proxy/data
+
+
+### Add cron script to call install script after reboot ###
+
+# Get the full working path
+working_path=$(pwd)
+script_path="$working_path/install.sh"
+
+# Add the script to crontab
+(crontab -l ; echo "@reboot $script_path") | crontab -
+
+#Reboot the server
+printf "\nServer install finished\n"
+printf "Rebooting"
+sleep 3
+
+sudo reboot
+
+
+
+
---
+++ 2install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#clearing Cron
+crontab -r
+
+#Installing tools
+printf "Instaling tools \n\n"
+sudo apt install curl git net-tools screen nmap
+
+printf "Installing docker \n\n"
+sudo apt install docker.io
+
+
+#Pulling images
+printf "Pulling images\n\n"
+sudo docker pull jc21/nginx-proxy-manager:latest
+sudo docker pull portainer/portainer-ce:latest
+sudo docker pull nginx:latest
+
+#Start machines
+printf "Spinning up machines"
+sudo docker run -d -p 8080:80 --name nginx --restart=always -v /usr/share/nginx/html/:/usr/share/nginx/html nginx:latest
+sudo docker run -d -p 80:80 -p 443:443 -p 81:81 --name ngx-proxy --restart=always -v /var/lib/docker/volumes/ngx-proxy/data:/data -v /var/lib/docker/volumes/ngx-proxy/letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest 
+sudo docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+#Instructions
+printf """\n
+Nginx: localhost:8080
+
+Proxy admin: localhost:81
+
+Portainer: localhost:9443
+
+"""
+
+printf """
+Nginx First time login:
+Email:    admin@example.com
+Password: changeme\n\n
+Good luck
+"""
+
